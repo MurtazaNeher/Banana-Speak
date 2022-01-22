c947af1304d2583b8dfc2323876d5c73ba6f7948
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-01-22 20:32:52 +0530

    Backend cmplt

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-Input");
+var txtOutput = document.querySelector("#txt-Output");
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Sorry !!! Server down , try again after some time")
+}
+
+function clickEventHandler() {
+    var inputTxt = txtInput.value;
+
+    fetch(getTranslationUrl(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            txtOutput.innerText = translatedTxt;
+        })
+        .catch(errorHandler)
+
+};
+
+
+
+
+btnTranslate.addEventListener("click", clickEventHandler)
